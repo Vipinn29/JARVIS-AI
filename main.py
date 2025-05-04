f659a03d4cd7973223e7588f203672d2d834a8d4
Vipinn29 <vipingupta2k4@gmail.com>
2025-05-04 21:10:02 +0530

    OpenAI Integrated

--- main.py
+++ main.py
@@ -1,14 +1,60 @@
 import speech_recognition as sr
-import openai
-import wikipedia as wk
+from openai import OpenAI
 import win32com.client as win
 import webbrowser
-import time
+import datetime
+import os
 
 speaker = win.Dispatch("SAPI.spVoice")
 
-print("Jarvis AI")
-speaker.Speak("Jarvis AI")
+chatStr = ""
+
+def chat(prompt):
+    global chatStr
+
+    client = OpenAI(
+        api_key="sk-vAn5ZActfdOlo3FREgPOThVc7UiedU6GmuACKjpyjAr7YmQK",
+        base_url="https://api.chatanywhere.tech/v1"
+    )
+
+    chatStr += f" User: {prompt}\n Jarvis: "
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": f" User: {prompt}\n Jarvis: "}
+        ],
+        logprobs=True,
+    )
+    
+    chatStr += f"{completion.choices[0].message.content}\n"
+    print(chatStr)
+    speaker.Speak(completion.choices[0].message.content)
+    return completion.choices[0].message.content
+
+
+def AI(prompt):
+    client = OpenAI(
+        api_key="sk-vAn5ZActfdOlo3FREgPOThVc7UiedU6GmuACKjpyjAr7YmQK",
+        base_url="https://api.chatanywhere.tech/v1"
+    )
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": f"{prompt}"}
+        ],
+        logprobs=True,
+    )
+
+    if not os.path.exists("OpenAI"):
+        os.mkdir("OpenAI")
+
+    with open(f"OpenAI/{''.join(prompt.split('AI')[1:]).strip()}.txt", "w") as f:  
+        f.write(f"OpenAI Response for Query: {prompt} \n-------------------------\n\n")
+        f.write(completion.choices[0].message.content)
+
 
 def TakeCommand():
     r = sr.Recognizer()
@@ -20,24 +66,53 @@ def TakeCommand():
             query = r.recognize_google(audio, language='en-in')
             print(f"User said: {query}\n")
             return query
-        except:
-            print("Some error occourd, Sorry From Jarvis")
+        except Exception as e:
+            print(f"Some error occourd, Sorry From Jarvis")
             return "None"
 
-while True:
-    query = TakeCommand()
+# Main Function
+if __name__ == '__main__':
+    print("Welcome to Jarvis AI")
+    speaker.Speak("Welcome to Jarvis AI")
 
-    sites = [["Youtube", "https://www.youtube.com/"],["google", "https://www.google.com/"],["wikipedia", "https://www.wikipedia.com/"],["instagram", "https://www.instagram.com/"]]
+    while True:
+        query = TakeCommand()
 
-    for site in sites:
-        if f"Open {site[0]}".lower() in query.lower():
-            speaker.Speak(f"Opening {site[0]}")
-            webbrowser.open(site[1])
+        # Add more Sites as well
+        sites = [["Youtube", "https://www.youtube.com/"],["google", "https://www.google.com/"],["wikipedia", "https://  www.wikipedia.com/"],["instagram", "https://www.instagram.com/"]]
 
-    Time = time.strftime("%H:%M:%S")
-    if "time" in query:
-        speaker.Speak(f"The time is: {Time}")
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                speaker.Speak(f"Opening {site[0]}")
+                webbrowser.open(site[1])
 
-    elif query == "quit":
-        break
+
+        Dt = datetime.datetime.now()
+        if "Time".lower() in query.lower():
+            speaker.Speak(f"The time is: {Dt.strftime('%X')}")
+
+
+        if "Date".lower() in query.lower():
+            speaker.Speak(f"The Date is: {Dt.strftime('%x')}")
+
+
+        if "Day".lower() in query.lower():
+            speaker.Speak(f"Today is: {Dt.strftime('%A')}")
+
+
+        elif "Using AI".lower() in query.lower():
+            AI(prompt=query)
+    
+
+        elif  "Jarvis quit".lower() in query.lower():
+            exit()
+
+
+        elif "reset chat".lower() in query.lower():
+            chatStr = ""
+
+
+        else:
+            print("Chatting...")
+            chat(query)
 
